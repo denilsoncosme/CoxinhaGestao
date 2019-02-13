66885d59a39bb3c113453be619077703646034dd
denilsoncosme <deni.cosme@gmail.com>
2019-02-12 22:19:15 -0200

    [modified] colocando outro caso de teste

--- CoxinhaGestao/Program.cs
+++ CoxinhaGestao/Program.cs
@@ -15,16 +15,15 @@ namespace CoxinhaGestao
             CalculoCoxinha calculoCoxinha = new CalculoCoxinha();
 
             Funcionario funcionario = new Funcionario("Nathalia", new CLT(), new DateTime(1994,02,03));
+            Console.WriteLine(funcionario.Nome + " - " + calculoCoxinha.GetCoxinha(funcionario));
 
+            funcionario = new Funcionario("Guilherme", new CLT(), new DateTime(1995, 06, 24));
             Console.WriteLine(funcionario.Nome + " - " + calculoCoxinha.GetCoxinha(funcionario));
 
             funcionario = new Funcionario("Leonardo", new Estagiario(), new DateTime(1999, 10, 22));
-
             Console.WriteLine(funcionario.Nome + " - " + calculoCoxinha.GetCoxinha(funcionario));
 
             funcionario = new Funcionario("Lúcio", new Consultor(), new DateTime(1992, 09, 01));
-
-
             Console.WriteLine(funcionario.Nome + " - " + calculoCoxinha.GetCoxinha(funcionario));
 
             Console.ReadKey();
